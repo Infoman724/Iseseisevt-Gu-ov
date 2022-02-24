f5e54b4da83efa2d91c01d27d8fc5fd5faa0d151
Infoman724 <93324312+Infoman724@users.noreply.github.com>
2022-02-24 16:16:22 +0200

    Добавьте файлы проекта.

---
+++ Iseseisevtöö Gužov/funktsionid.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+
+namespace Iseseisevtöö_Gužov
+{
+    class funktsionid
+    {
+        public static void ulesane1()
+        {
+            Console.WriteLine("kirjuta midagi");
+            string line = Console.ReadLine();
+            string vowels = "aeouiy";
+            int result = 0;
+            foreach (char symbol in line)
+            {
+                if (vowels.Contains(symbol.ToString()))
+                    result++;
+            }
+            Console.WriteLine(result);
+            double pikkusDouble = Convert.ToDouble(line.Length);
+            double resultDouble = Convert.ToDouble(result);
+            double protsent = (resultDouble / pikkusDouble) * 100;
+            Console.WriteLine($"Esinemisprotsent on {protsent}%");
+
+        }
+        public static void ulesane2()
+        {
+            Console.Write("n = ");
+            var n = int.Parse(Console.ReadLine());
+            var a = new int[n];
+            var r = new Random();
+            var count = 0;
+            for (var i = 0; i < n; i++)
+            {
+                a[i] = r.Next(-2000, 2000);
+                if (a[i] > -100 && a[i] < 100) count++;
+            }
+            Console.WriteLine("{0} numbrid vahemikus -100 kuni 100", count);
+
+        }
+        public static void ulesane3()
+        {
+            
+        }
+
+
+
+
+    }
+}
---
+++ Iseseisevtöö Gužov/ülessaned.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Iseseisevtöö_Gužov
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Mul on 5 variant! Milles ülesane tahab kontrollida? ül1,ül2,ül3?");
+            string vastus = Console.ReadLine().ToLower();
+            while (true)
+            {
+                if (vastus == "ül1")
+                {
+                    funktsionid.ulesane1();
+                }
+                else if (vastus == "ül2")
+                {
+                    funktsionid.ulesane2();
+                }
+                else if (vastus == "ül3")
+                {
+
+                }
+                else  
+                {
+                    Console.Write("Midagi läks valesti");
+                    break;
+                }
+            
+            
+            
+            
+            }
+            
+        }
+    }
+}
